89c06438ee319cd18362dfdc9c70afcf49a2b1d8
karthik <karthiktumala143@gmail.com>
2024-03-05 17:30:23 +0530

    logout

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const jwt = require('jsonwebtoken');
-const cors = require('cors')
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const app = express();
-const SECRET_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWUxYTkxNzdkMGQ2YWI3N2VmZGQxYWMiLCJ1c2VybmFtZSI6ImthcnRoaWsiLCJpYXQiOjE3MDkyOTAyNDIsImV4cCI6MTcwOTI5Mzg0Mn0.RoaUt2IQKxD55PasZWN7avHc55ytfPGptzOnvq1gENU"
+const SECRET_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWUxYTkxNzdkMGQ2YWI3N2VmZGQxYWMiLCJ1c2VybmFtZSI6ImthcnRoaWsiLCJpYXQiOjE3MDkyOTAyNDIsImV4cCI6MTcwOTI5Mzg0Mn0.RoaUt2IQKxD55PasZWN7avHc55ytfPGptzOnvq1gENU";
 app.use(express.json());
 
 app.use(cors());
@@ -23,6 +24,7 @@ mongoose
 
 const postSchema = mongoose.Schema({
   post: { type: String, require: true },
+  author:{type:String,require:true},
   created_by: { type: String, require: true },
   created_date: { type: Date, default: Date.now() },
   update_date: { type: Date, default: Date.now() },
@@ -38,19 +40,20 @@ const posts = mongoose.model("posts", postSchema);
 const user = mongoose.model("user", userSchema);
 
 // token verify middle ware
-function verifyJWT(req,res,next){
-  const token = req.headers['authorization'].split(" ")[1];
+function verifyJWT(req, res, next) {
+  console.log(req.headers, "headers");
+  const token = req.headers["authorization"].split(" ")[1];
   if (!token) {
-    return res.status(403).json({ message: 'Token Not Provided' });
+    return res.status(403).json({ message: "Token Not Provided" });
   }
 
-  jwt.verify(token,SECRET_KEY,(err,decode)=>{
+  jwt.verify(token, SECRET_KEY, (err, decode) => {
     if (err) {
-      return res.status(401).json({ message: 'Invalid token' });
+      return res.status(401).json({ message: "Invalid token" });
     }
     req.user = decode;
     next();
-  })
+  });
 }
 
 // post routes
@@ -58,29 +61,30 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "REST FULL" });
 });
 
-app.get("/post/all", async(req, res) => {
+app.get("/post/all", async (req, res) => {
   try {
     let allposts = await posts.find({});
-    res.status(200).json({posts:allposts})
+    res.status(200).json({ posts: allposts });
   } catch (error) {
-    res.status(500).json({message:"Internal Error !"})
+    res.status(500).json({ message: "Internal Error !" });
   }
 });
 
-app.post("/post/add",verifyJWT, async(req, res) => {
+app.post("/post/add", verifyJWT, async (req, res) => {
   try {
-    let {userId} = req.user;
-    let {post} = req.body;
-    if(userId){
+    let { userId } = req.user;
+    let { post,author } = req.body;
+    if (userId) {
       let newPost = {
         post,
+        author,
         created_by: req.user.userId,
         created_date: Date.now(),
         update_date: Date.now(),
-      }
+      };
       await posts.create(newPost);
       res.status(200).json({ message: "post added success !" });
-    }else{
+    } else {
       res.status(400).json({ message: "Internal Error !" });
     }
   } catch (error) {
@@ -121,9 +125,14 @@ app.post("/account/signin", async (req, res) => {
     let userExists = await user.findOne({ email: email });
     if (userExists) {
       if (userExists.password === password) {
-        const token = jwt.sign({ userId: userExists._id, username: userExists.name }, SECRET_KEY, { expiresIn: '1m' });
-        res.status(200).json( {id:userExists._id,name:userExists.name,token}
-         );
+        const token = jwt.sign(
+          { userId: userExists._id, username: userExists.name },
+          SECRET_KEY,
+          { expiresIn: "1h" }
+        );
+        res
+          .status(200)
+          .json({ id: userExists._id, name: userExists.name, token });
       } else {
         res.status(401).json({ message: "wrong password" });
       }
@@ -149,15 +158,28 @@ app.post("/account/signup", async (req, res) => {
   }
 });
 
-app.get('/protected', verifyJWT, (req, res) => {
+app.get("/protected", verifyJWT, (req, res) => {
   res.json({ message: req.user });
 });
 
-
-app.get('/token',verifyJWT,(req,res)=>{
-  if(req.user){
-    res.status(200).json({...req.user})
-  }else{
-    res.status(201).json({message:"login failed"})
+app.get("/token", verifyJWT, (req, res) => {
+  if (req.user) {
+    res.status(200).json({ ...req.user });
+  } else {
+    res.status(201).json({ message: "login failed" });
   }
-})
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    let { id } = req.params;
+    let result = await user.findOne({ _id: id });
+    if (result) {
+      res.status(200).json({ message: result });
+    } else {
+      res.status(400).json({ message: "no user found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "internal error" });
+  }
+});
